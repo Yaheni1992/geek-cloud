f6e0be433e4b4af5863f09275b3252f521682a55
Yaheni1992 <neo_jeka@mail.ru>
2022-05-30 19:10:15 +0300

    lesson1

--- src/main/java/com/geekbrains/cloud_2022_june/geekcloud/ChatController.java
+++ src/main/java/com/geekbrains/cloud_2022_june/geekcloud/ChatController.java
@@ -1,14 +1,47 @@
 package com.geekbrains.cloud_2022_june.geekcloud;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatController implements Initializable {
     @FXML
-    private Label welcomeText;
+    public ListView<String> clientView;
+    @FXML
+    public ListView<String> serverView;
+    private Network network;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField textView;
+    @FXML
+    public ListView<String> listView;
+
+    private void readLoop() {
+        try {
+            while (true) {
+                String msg = network.readMessage();
+                Platform.runLater(() -> clientView.getItems().add(msg));
+            }
+        } catch (Exception e) {
+            System.err.println("Соединение потеряно");
+        }
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            network = new Network(8189);
+            Thread readThread = new Thread(this::readLoop);
+            readThread.setDaemon(true);
+            readThread.start();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
